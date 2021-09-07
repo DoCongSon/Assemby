1debd841d87fdc39d4b7d3c513e3ac0507f0279b
Đỗ Công Sơn <sonanhson99@gmail.com>
2021-09-07 22:18:03 +0700

    Update bai_7_tong.asm

--- src/bai_7_tong.asm
+++ src/bai_7_tong.asm
@@ -14,16 +14,16 @@
         LEA DX, B1
         MOV AH, 9
         INT 21h
-        MOV AH, 1    ; nhap so nhu 1
+        MOV AH, 1    ; nhap so thu 1
         INT 21h
         MOV BL, AL   ; luu vao BH
         
         LEA DX, B2
         MOV AH, 9
         INT 21h
-        MOV AH, 1    ; nhap so nhu 2
+        MOV AH, 1    ; nhap so thu 2
         INT 21h
-        ADD AL, BL   ; cong 2 so
+        ADD AL, BL   ; cong 2 so ket qua dua lai vao AL
         MOV AH, 30h  ; dat AH = 30h (ma ascii cua so 0)
         AAA          ; hieu chinh kq. neu lon > 10 thi AH tang len 1 don vi, AL chua so hang don vi 
         ADD AL ,30h  ; chuyen AL ve ma ascii
@@ -36,7 +36,7 @@
         MOV AH, 9
         INT 21h
         MOV AH, 2
-        CMP BH, 30h  ; so sanh BH voi 30h                     ;  
+        CMP BH, 30h  ; so sanh BH voi 30h (so hang chuc bang 0)                    ;  
         JZ T1        ; neu BH = 30h thi CF = 1. luc do nhay toi T1
         MOV DL, BH   ; in so hang chuc
         INT 21h
